841133583b05772524223cb0afa231f9aa5f7af8
Francis <francisangeles3@hotmail.com>
2016-05-31 19:41:54 -0400

    Initial commit

---
+++ src/main/java/Estudiante.java
@@ -0,0 +1,58 @@
+
+/**
+* Created by Francis Cáceres on 30/5/2016.
+        */
+public class Estudiante {
+
+    private int matricula;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String carrera;
+
+
+    public Estudiante() {
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+}
+
---
+++ src/main/java/main.java
@@ -0,0 +1,56 @@
+/**
+ * Created by Francis Cáceres on 29/5/2016.
+ */
+
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class main {
+    public static void main(String [] args) throws Exception{
+
+        Class.forName("org.h2.Driver");
+        creandoTabla();
+
+        //staticFileLocation("/");
+        get("/", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello Francis, let me suck you!");
+
+            return new ModelAndView(attributes, "/inicio.ftl");
+        }, new FreeMarkerEngine());
+
+    }
+    public static void creandoTabla() throws SQLException{
+        String sql = "CREATE TABLE IF NOT EXISTS ESTUDIANTE\n" +
+                "(\n" +
+                "  MATRICULA INTEGER PRIMARY KEY NOT NULL,\n" +
+                "  NOMBRE VARCHAR(50) NOT NULL,\n" +
+                "  APELLIDO VARCHAR(50) NOT NULL,\n" +
+                "  TELEFONO VARCHAR(25) NOT NULL,\n" +
+                "  CARRERA VARCHAR(50) NOT NULL\n" +
+                ");";
+        try {
+            Connection con = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/practica2", "sa", "");
+            Statement statement = con.createStatement();
+            statement.execute(sql);
+            statement.close();
+            con.close();
+        } catch( SQLException e ){
+            System.out.print( e.getMessage() );
+        }
+    }
+
+}
+
+
+
